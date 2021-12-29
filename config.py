e25f3f7f40fe0c6a1f8033f72ad9d0a1d96f8d93
Enkh-Undral EnkhBayar <484970@mail.muni.cz>
2021-12-29 20:54:21 +0100

    add functional config

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 login_url = "https://is.muni.cz/auth"
 student_page = "https://is.muni.cz/auth/student/"
 
-lecture_index = 5
-path_to_driver = r"E:\stuff\skola\muni\chromedriver.exe"
-UCO = "523375"
-primary_password = "5JMSebGN"
+lecture_index = 1
+path_to_driver = r"fill me in"
+UCO = "fill me in"
+primary_password = "fill me in"
